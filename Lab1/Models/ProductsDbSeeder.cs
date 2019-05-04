d0cfa7c442f12b8df952a23090ea0185d53a305e
midee <ana.midee@yahoo.com>
2019-05-04 12:40:47 +0300

    Lab1

---
+++ Lab1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class ProductsDbSeeder
+    {
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Masina",
+                    Description = "Family",
+                    Category = "Sport",
+                    Price = 100.5
+                },
+                new Product
+                {
+                    Name = "Telefon",
+                    Description = "Smart",
+                    Category = "Small",
+                    Price = 200.5
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
+
